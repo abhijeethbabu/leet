d1d037e42b24b568624172ec15316046b3b400a9
abhijeethbabu <106905916+abhijeethbabu@users.noreply.github.com>
2022-06-27 15:28:22 +0530

    Update trailingZeros.py
    code cleanup

--- trailingZeros.py
+++ trailingZeros.py
@@ -5,13 +5,6 @@ import math
 class Solution:
     def trailingZeroes(self, n: int) -> int:
         # no of zeroes == power of 10 == ( pow of 5) + ( pow of 25) + ..
-        # a = 0
-        # val = len(str(n))+1
-        # for p in range(1,val+1):
-        #     for i in range(1,n+1):
-        #         if i % pow(5,p) == 0:
-        #             a += 1
-        # return a
         a = 0
         p = 5
         while n//p != 0:
